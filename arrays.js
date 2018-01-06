52ba0443ea949969a91051d96e8aea0bf6582585
Denise Pendleton <dpendl1@hotmail.com>
2018-01-06 20:17:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,5 +16,12 @@ function addElementToEndOfArray (array, element){
 function destructivelyAddElementToEndOfArray(array, element){ 
   array.push(element);
   return array
+
+}
+
+function accessElementInArray (array,index){
+  return index[]
   
 }
+
+
