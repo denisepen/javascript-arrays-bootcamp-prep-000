4f5c490849bd7f2bbeb94d880ca2153b1217992d
Denise Pendleton <dpendl1@hotmail.com>
2018-01-06 20:23:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,5 +23,9 @@ function accessElementInArray (array,index){
   return array[index]
   
 }
-
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+  
+}
 
